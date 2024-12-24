bc5fc5c10d38cece2825077fa23e474705032f4a
Kazi Rayhan Reza Tirtho <thisiskazi@gmail.com>
2024-12-24 12:56:32 +0600

    donne

--- resources/views/pages/thank-you.blade.php
+++ resources/views/pages/thank-you.blade.php
@@ -1,20 +1,11 @@
 <x-app>
-    <!-- BREADCRUMB AREA -->
-    <section class="rts-breadcrumb breadcrumb-height breadcumb-bg"
-        style="background-image: url(assets/images/banner/breadcrumb.jpg);">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="breadcrumb-content">
-                        <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="/">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Thank You</li>
-                        </ul>
-                        <h2 class="section-title">Thank you</h2>
-                        <p class="text-light">Thank you for you application</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+   <div  class="container vh-100 d-flex justify-content-center align-items-center flex-column">
+    <h1>
+        Thank you
+    </h1>
+    <p>
+        We have received you application you will get to hear from us soon
+    </p>
+    <a style="text-decoration: underline" href="{{url('/')}}">Go back home</a>
+   </div>
 </x-app>
